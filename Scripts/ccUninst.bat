61c00a236b928bf2f4ca1fdc56e80317b20cb776
Torkvato <andreuccio@mail.ru>
2025-01-13 21:21:31 +0300

    Create ccUninst.bat

---
+++ Scripts/ccUninst.bat
@@ -0,0 +1,7 @@
+del "General\ccInit.lua"
+del "General\mawItemSorter.lua" 
+del "General\ccConvenience.lua" 
+del "General\ccInfo.lua" 
+del "General\ccCombatlog.lua"
+del "Global\ccTravelSchedule.lua"
+
