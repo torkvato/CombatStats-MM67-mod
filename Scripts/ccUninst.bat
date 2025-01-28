f0cfc5a1da530fbf509da1a968ed56f573cf71b7
Torkvato <andreuccio@mail.ru>
2025-01-28 20:29:52 +0300

    Update ccUninst.bat

--- Scripts/ccUninst.bat
+++ Scripts/ccUninst.bat
@@ -3,5 +3,9 @@ del "General\mawItemSorter.lua"
 del "General\ccConvenience.lua" 
 del "General\ccInfo.lua" 
 del "General\ccCombatlog.lua"
+del "General\ccBuffExpirationAlert.lua"
+
 del "Global\ccTravelSchedule.lua"
+del "Global\ccTrainingDummy.lua"
+
 
